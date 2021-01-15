6dc50af9ba701dd1214c1f27a726820c8369c84e
MalikovB <76504900+MalikovB@users.noreply.github.com>
2021-01-15 17:07:37 -0500

    Add files via upload

---
+++ hangMan/fileReader.js
@@ -0,0 +1,11 @@
+var letterbox = [];
+
+letterbox['a'] = document.getElementById('a');
+letterbox['b'] = document.getElementById('b');
+letterbox['c'] = document.getElementById('c');
+letterbox['d'] = document.getElementById('d');
+letterbox['e'] = document.getElementById('e');
+letterbox['f'] = document.getElementById('f');
+letterbox['g'] = document.getElementById('g');
+letterbox['h'] = document.getElementById('h');
+letterbox['i'] = document.getElementById('i');
